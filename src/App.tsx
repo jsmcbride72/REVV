020118e0f77087ad3773551282fb81ca5f1071da
jsmcbride72 <34973242+jsmcbride72@users.noreply.github.com>
2025-12-24 07:13:23 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ function App() {
       images: [
         '/phones-spin.mov',
         '/main-tile-mock.png',
+        '/mobile-preview.png',
         'https://images.pexels.com/photos/4391470/pexels-photo-4391470.jpeg?auto=compress&cs=tinysrgb&w=800',
         'https://images.pexels.com/photos/6457579/pexels-photo-6457579.jpeg?auto=compress&cs=tinysrgb&w=800'
       ],
